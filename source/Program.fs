de8f925187d447bc17c1b40bc44e428c65a3e8ed
Matt Salmon <matt@iua.co.za>
2017-10-06 15:46:09 +0200

    Basic F# syntax

---
+++ source/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    let x = "Hello World"
+    Console.WriteLine(x)
+    let y = Console.ReadKey()
+    0 // return an integer exit code
---
+++ source/Script1.fsx
@@ -0,0 +1,22 @@
+﻿let x = "Hello World"
+let ints = [|1;2;3;4;5;6|]
+let multipleByTwo x = x * 2
+let multiplied = ints |> Array.map (fun i -> i * 3)
+let multiplied' = ints |> Array.map (fun i -> multipleByTwo i)
+
+let isEven x = 
+    match x % 2 = 0 with
+    | true -> "even"
+    | false -> "odd"
+let multipliedAndIsEven = 
+    ints
+    |> Array.map (fun i -> multipleByTwo i)
+    |> Array.map (fun i -> isEven i)
+
+let a2 = isEven 5
+
+
+
+
+
+
