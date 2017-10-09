789e63d9158bc6760caf8b2ec35a32544308871b
Matt Salmon <matt@iua.co.za>
2017-10-09 16:04:17 +0200

    Chapter 2 page 33 linear regression

--- Chapter2/Script.fsx
+++ Chapter2/Script.fsx
@@ -11,6 +11,7 @@ let variance (source:float seq) =
     let deltas = Seq.map (fun x -> pown (x-mean) 2) source
     Seq.average deltas
 
+// standard deviation - a quantity expressing by how much the members of a group differ from the mean value for the group
 let standardDeviation values = 
     sqrt (variance values)
 
@@ -21,5 +22,26 @@ let mX = Seq.average x
 let mY = Seq.average y
 let sX = standardDeviation x
 let sY = standardDeviation y
+
+// Pearson's correlation - a measure of the linear correlation between two variables X and Y. 
+// It has a value between +1 and −1, where 1 is total positive linear correlation, 0 is no linear correlation, and −1 is total negative linear correlation.
+let pearsonsCorrelation(a:float seq, b:float seq) = 
+    let mX = Seq.average a
+    let mY = Seq.average b
+    
+    let x = a |> Seq.map (fun x -> x - mX)
+    let y = b |> Seq.map (fun y -> y - mY)
+
+    let xys = Seq.zip x y
+    let xy = xys |> Seq.map (fun (x, y) -> x*y, x*x, y*y)
+    let sxy = xy |> Seq.sumBy (fun (xy, x2, y2) -> xy)
+    let sx2 = xy |> Seq.sumBy (fun (xy, x2, y2) -> x2)
+    let sy2 = xy |> Seq.sumBy (fun (xy, x2, y2) -> y2)
+    sxy / sqrt (sx2*sy2)
+
+let r = pearsonsCorrelation (x, y)
+
+let b = r*(sY/sX)
+let A = mY - b*mX
 
 
